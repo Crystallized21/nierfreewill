5a932f43670413b5fc3ddc820d7382444ff75cb2
Michael Bui <michael.bui_crystal@outlook.com>
2025-03-06 20:50:11 +1300

    e

--- src/components/pages/TestSite.tsx
+++ src/components/pages/TestSite.tsx
@@ -4,7 +4,7 @@ const TestSite = () => {
   return (
     <div>
       <h1 className="text-white">Test Site</h1>
-      <ErrorPopup text="Error: Systems Abnormal"/>
+      <ErrorPopup text="Error: Systems Abnormal" x={2} y={2}/>
     </div>
   );
 };
