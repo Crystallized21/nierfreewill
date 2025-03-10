ce4078b8040c27c0f9a491b63791c53b6f8cdf3f
Michael Bui <michael.bui_crystal@outlook.com>
2025-03-10 21:42:08 +1300

    update osquest page to implment all the new things i guess

--- src/components/pages/os/OSQuest.tsx
+++ src/components/pages/os/OSQuest.tsx
@@ -1,41 +1,78 @@
+import {Outlet, useParams, useSearchParams} from "react-router-dom";
 import PagesTemplate from "../../PagesTemplete.tsx";
-import {Bar} from "../../uiElements/osInterface/Bar.tsx";
-import {Button} from "../../uiElements/osInterface/Button.tsx";
-import styles from "./Map.module.scss";
+import {YorhaNavLink} from "../../uiElements/osInterface/NavBar/YorhaNavLink.tsx";
+import {SubTitle} from "../../../utils/ParamAsSubtitle.ts";
+import PagesChildTemplate from "../../PagesChildTemplate.tsx";
+import StatusModule from "../../uiElements/osInterface/StatusModule/StatusModule.tsx";
 import OSstyles from "./OS.module.scss";
 
-const OsMap = () => {
+const QuestList = [
+  {
+    Link: "/os/quest/Active/Quest5?status=",
+    Text: "Active Quests",
+    type: "active",
+  },
+  {
+    Link: "/os/quest/all/Quest1?status=",
+    Text: "All Quests",
+    type: "",
+  },
+  {
+    Link: "/os/quest/cleared/Quest1?status=",
+    Text: "Cleared Quests",
+    type: "cleared",
+  },
+]
+
+const OSQuest = () => {
+
+  const param = useParams();
+  const [searchParams] = useSearchParams();
+  const status = (searchParams.get("status"));
+
+  const ParamCheck = () => {
+    if (status === "active") {
+      return "quests currently in progress";
+    } else if (status === "cleared") {
+      return "completed quests";
+    } else if (!status) {
+      return "all quests";
+    } else if (status === "")
+      return "all accepted quests";
+  }
+
+  const TypeCheck = () => {
+    if (status === "") {
+      return "all"
+    } else
+      return param.statusType
+  }
+
   return (
     <div className={OSstyles.MainContent}>
       <PagesTemplate
-        title="QUESTS"
-        child={<div className={styles.ContentContainer}>
-          <div className={styles.LeftPanel}>
-            <div className={styles.LeftPanelChild}>
-              <div className={styles.Bar}>
-                <Bar/>
-              </div>
-              <div className={styles.Button}>
-                <Button disabled={false} text="Active Quests"/>
-                <Button disabled={false} text="All Quests"/>
-                <Button disabled={false} text="Cleared Quests"/>
-              </div>
-            </div>
-            <div className={styles.LeftPanelFooter}>
-              {/* footer panel  */}
-            </div>
-          </div>
-          <div className={styles.RightPanel}>
-            <div className={styles.MapPanel}>
-              <h1>
-                Classified
-              </h1>
-            </div>
-          </div>
-        </div>}
-        footer="Map Footer"/>
+        title={`QUESTS`}
+        subtitle={SubTitle(TypeCheck(), "Quest")}
+        footer={`View ${ParamCheck()}.`}
+        child={
+          <PagesChildTemplate
+            LeftContent={
+              <>
+                {QuestList.map((item) => (
+                  <YorhaNavLink key={item.Link} to={item.Link} filter={item.type} filterType={"status"}
+                                text={item.Text}/>
+                ))}
+              </>
+            }
+            Outlet={<Outlet/>}
+            RightContent={
+              <StatusModule/>
+            }
+          />
+        }
+      />
     </div>
-  );
-};
+  )
+}
 
-export default OsMap;
+export default OSQuest;
