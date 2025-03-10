538ae9dcaa827736fdfa8d604855017a81441033
Michael Bui <michael.bui_crystal@outlook.com>
2025-03-10 20:31:24 +1300

    fix more shit

--- src/components/pages/TestSite.tsx
+++ src/components/pages/TestSite.tsx
@@ -1,5 +1,6 @@
 import ErrorPopup from "../uiElements/error/ErrorPopup.tsx";
 import {NavbarModule} from "../modules/NavBarModule.tsx";
+import {YorhaNavLink} from "../uiElements/osInterface/NavBar/YorhaNavLink.tsx";
 
 const TestSite = () => {
   return (
@@ -7,6 +8,7 @@ const TestSite = () => {
       <h1 className="text-white">Test Site</h1>
       <ErrorPopup text="Error: Systems Abnormal" x={2} y={2}/>
       <NavbarModule/>
+      <YorhaNavLink/>
     </div>
   );
 };
