d56bb5cc01dd5689dd054f47506c02c1d29a84a9
Michael Bui <michael.bui_crystal@outlook.com>
2025-03-10 14:21:38 +1300

    fuck the css a lil

--- src/components/pages/os/OSMap.tsx
+++ src/components/pages/os/OSMap.tsx
@@ -10,32 +10,32 @@ const OsMap = () => {
     <div className={OSstyles.App}>
       <NavbarModule/>
       <PagesTemplate
-      title="MAP"
-      child={<div className={styles.ContentContainer}>
-        <div className={styles.LeftPanel}>
-          <div className={styles.LeftPanelChild}>
-            <div className={styles.Bar}>
-              <Bar/>
+        title="MAP"
+        child={<div className={styles.ContentContainer}>
+          <div className={styles.LeftPanel}>
+            <div className={styles.LeftPanelChild}>
+              <div className={styles.Bar}>
+                <Bar/>
+              </div>
+              <div className={styles.Button}>
+                <Button disabled={true} text="Quick Save"/>
+                <Button disabled={true} text="Map Mode"/>
+                <Button disabled={true} text="Map Icons"/>
+              </div>
             </div>
-            <div className={styles.Button}>
-              <Button disabled={true} text="Quick Save"/>
-              <Button disabled={true} text="Map Mode"/>
-              <Button disabled={true} text="Map Icons"/>
+            <div className={styles.LeftPanelFooter}>
+              {/* footer panel  */}
             </div>
           </div>
-          <div className={styles.LeftPanelFooter}>
-            {/* footer panel  */}
+          <div className={styles.RightPanel}>
+            <div className={styles.MapPanel}>
+              <h1>
+                Classified
+              </h1>
+            </div>
           </div>
-        </div>
-        <div className={styles.RightPanel}>
-          <div className={styles.MapPanel}>
-            <h1>
-              Classified
-            </h1>
-          </div>
-        </div>
-      </div>}
-      footer="Map Footer"/>
+        </div>}
+        footer="Map Footer"/>
     </div>
   );
 };
