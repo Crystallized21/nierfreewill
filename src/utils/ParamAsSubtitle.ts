f93b718e21c650f3728618a1127128b87a20f920
Michael Bui <michael.bui_crystal@outlook.com>
2025-03-10 21:41:27 +1300

    add util to deal with subtitles

---
+++ src/utils/ParamAsSubtitle.ts
@@ -0,0 +1,15 @@
+export function SubTitle(ParamType: string | undefined, ParamTail: string) {
+  function capitalizeFirstLetter(string: string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  let subTitle = (`${ParamType} ${ParamTail}`);
+  let strip = "- ";
+  if (ParamType === undefined) {
+    subTitle = "";
+    strip = ""
+  }
+  return (
+    strip + capitalizeFirstLetter(subTitle)
+  )
+}
