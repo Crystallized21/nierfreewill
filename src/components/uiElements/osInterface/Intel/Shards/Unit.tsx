2275fe34bf064de332971803d28de4b046d51697
Michael Bui <michael.bui_crystal@outlook.com>
2025-03-11 13:28:24 +1300

    Add unit data

--- src/components/uiElements/osInterface/Intel/Shards/Unit.tsx
+++ src/components/uiElements/osInterface/Intel/Shards/Unit.tsx
@@ -1,7 +1,42 @@
+import {useParams} from "react-router-dom";
+import {getArchivesMockID, getNestedArchivesMockID} from "../../../../../utils/ArchivesMockData.ts";
+
 const Unit = () => {
+  const params = useParams();
+  const paramType = params.type;
+
+  const intelListId = getArchivesMockID(paramType, parseInt(params.intelid as string));
+  const secondId = getNestedArchivesMockID(paramType, parseInt(params.intelid as string));
+
+  const data = intelListId || secondId;
+
+  console.log(data?.content.map((items) => items));
+
+  const mapCheck = () => {
+    if (!Object.prototype.hasOwnProperty.call(data || {}, "content")) {
+      return <>weird, it seems the unit data is empty</>;
+    }
+
+    switch (true) {
+      case Object.prototype.hasOwnProperty.call(data, "content"):
+        return (
+          <>
+            {data?.content.map((items, index) =>
+              <div key={index + 1}>
+                {typeof items === 'string' ? items : JSON.stringify(items)}
+                <br/>
+              </div>
+            )}
+          </>
+        );
+      default:
+        return <>weird, it seems the unit data is empty</>;
+    }
+  };
+
   return (
     <>
-      Unit
+      {mapCheck()}
     </>
   );
 };
