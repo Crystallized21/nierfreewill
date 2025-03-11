253b45fc89f70e391b2807ca45d60e5253ec7590
Michael Bui <michael.bui_crystal@outlook.com>
2025-03-11 16:58:31 +1300

    add animations to bar

--- src/components/uiElements/osInterface/Bar.tsx
+++ src/components/uiElements/osInterface/Bar.tsx
@@ -1,5 +1,6 @@
 import styled, {ThemeProvider} from 'styled-components';
 import colors from './colors.json';
+import {motion} from 'framer-motion';
 
 interface BarProps {
   dark?: boolean;
@@ -9,12 +10,13 @@ const BarParent = styled.div`
     display: flex;
     flex-direction: row;
     height: 100%;
+    align-items: center;
 `;
 
-const BarItem = styled.div`
+const BarItem = styled(motion.div)`
     background-color: ${props => props.theme.main};
     width: 10px;
-    height: 100%;
+    height: 0;
     margin-right: 5px;
 
     &:nth-child(2) {
@@ -37,15 +39,35 @@ export const Bar = ({dark = false, ...props}: BarProps) => {
     if (dark === false) {
       return (
         <>
-          <BarItem/>
-          <BarItem/>
+          <BarItem
+            initial={{height: 0, opacity: 0}}
+            animate={{height: '100%', opacity: 1}}
+            exit={{opacity: 0}}
+            transition={{duration: 0.25, ease: [.1,.6,0,1]}}
+          />
+          <BarItem
+            initial={{height: 0, opacity: 0}}
+            animate={{height: '100%', opacity: 1}}
+            exit={{opacity: 0}}
+            transition={{duration: 0.25, ease: [.1,.6,0,1]}}
+          />
         </>
       );
     } else if (dark === true) {
       return (
         <ThemeProvider theme={theme}>
-          <BarItem/>
-          <BarItem/>
+          <BarItem
+            initial={{height: 0, opacity: 0}}
+            animate={{height: '100%', opacity: 1}}
+            exit={{opacity: 0}}
+            transition={{duration: 0.25, ease: [.1,.6,0,1]}}
+          />
+          <BarItem
+            initial={{height: 0, opacity: 0}}
+            animate={{height: '100%', opacity: 1}}
+            exit={{opacity: 0}}
+            transition={{duration: 0.25, ease: [.1,.6,0,1]}}
+          />
         </ThemeProvider>
       );
     }
