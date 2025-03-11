812bc1bdf27cbe1d354b967832f97ef265fd022c
Michael Bui <michael.bui_crystal@outlook.com>
2025-03-11 16:28:52 +1300

    format

--- src/components/pages/os/OSMap.tsx
+++ src/components/pages/os/OSMap.tsx
@@ -9,30 +9,31 @@ const OsMap = () => {
     <div className={OSstyles.MainContent}>
       <PagesTemplate
         title="MAP"
-        child={<div className={styles.ContentContainer}>
-          <div className={styles.LeftPanel}>
-            <div className={styles.LeftPanelChild}>
-              <div className={styles.Bar}>
-                <Bar/>
+        child={
+          <div className={styles.ContentContainer}>
+            <div className={styles.LeftPanel}>
+              <div className={styles.LeftPanelChild}>
+                <div className={styles.Bar}>
+                  <Bar/>
+                </div>
+                <div className={styles.Button}>
+                  <Button disabled={true} text="Quick Save"/>
+                  <Button disabled={true} text="Map Mode"/>
+                  <Button disabled={true} text="Map Icons"/>
+                </div>
               </div>
-              <div className={styles.Button}>
-                <Button disabled={true} text="Quick Save"/>
-                <Button disabled={true} text="Map Mode"/>
-                <Button disabled={true} text="Map Icons"/>
+              <div className={styles.LeftPanelFooter}>
+                {/* footer panel  */}
               </div>
             </div>
-            <div className={styles.LeftPanelFooter}>
-              {/* footer panel  */}
+            <div className={styles.RightPanel}>
+              <div className={styles.MapPanel}>
+                <h1>
+                  Classified Information
+                </h1>
+              </div>
             </div>
-          </div>
-          <div className={styles.RightPanel}>
-            <div className={styles.MapPanel}>
-              <h1>
-                Classified Information
-              </h1>
-            </div>
-          </div>
-        </div>}
+          </div>}
         footer="nothingisrealnothingisrealnothingisrealnothingisrealnothingisrealnothingisrealnothingisrealnothingisrealnothingisrealnothingisrealnothingisrealnothingisrealnothingisrealnothingisrealnothingisrealnothingisrealnothingisrealnothingisreal`"/>
     </div>
   );
