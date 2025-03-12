a66fac0d4dcfd7b0cf18c828b2df326e666cb5c0
Michael Bui <michael.bui_crystal@outlook.com>
2025-03-12 16:57:54 +1300

    update sound effects

--- src/hooks/useSoundEffects.ts
+++ src/hooks/useSoundEffects.ts
@@ -1,10 +1,13 @@
 import useSound from 'use-sound';
 import hoverSfx from "../assets/audio/cursor.mp3";
 import confirmSfx from "../assets/audio/confirm.mp3";
+import errorSfx from '../assets/audio/error.mp3';
 
-export const useSoundEffects = (clickSound?: string) => {
+
+export const useSoundEffects = (clickSound?: string, errorSound?: string) => {
   const [playHover] = useSound(hoverSfx);
   const [playConfirm] = useSound(clickSound || confirmSfx);
+  const [playError] = useSound(errorSound || errorSfx);
 
-  return { playHover, playConfirm };
+  return { playHover, playConfirm, playError};
 };
