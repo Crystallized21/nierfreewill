7659498deca1b48dfa2da13a9691b209c863099e
Michael Bui <michael.bui_crystal@outlook.com>
2025-03-14 10:42:54 +1300

    remove todo and format

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,12 +1,9 @@
 import HomeScrollIntro from "../creditsScroll/HomeScrollIntro.tsx";
 
-// TODO: Add a scroll indicator to the bottom of the page
-
 export default function Home() {
   return (
     <div className="bg-black">
       <HomeScrollIntro/>
     </div>
   );
-
 }
